f407acd207a95e08b2ca0bd0eb32cd90aaab3a35
Anup <anupisri107@gmail.com>
2023-05-13 22:54:22 +0530

    server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.render(`index.ejs`);
+});
+
+// ROUTE
+app.get("/login", (req, res) => {});
+app.listen(3030);
