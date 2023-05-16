bcb508ce29e2bfc130fe0f0759e72ecdb22d49d4
Anup <anupisri107@gmail.com>
2023-05-16 13:04:12 +0530

    Done

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
+if (process.env.NODE_ENV !== "production ") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const initializePassport = require("./passport-config");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 const users = [];
 
 app.use(express.urlencoded({ extended: false })); // help to access the data from the  which are coming through the post method
 
-app.post("/register", async (req, res) => {
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // we wont resave the session variable if nothing is changed
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+
+// configuration for the loing post functionailty
+app.post(
+  "/login",
+  checkUnAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+//configuration for the register post functionailty
+app.post("/register", checkUnAuthenticated, async (req, res) => {
   try {
     const hashPass = await bcrypt.hash(req.body.password, 10); // we are converting the passward into the hash password(decrypted form )
     // Pushing the data in the users array
@@ -27,18 +64,39 @@ app.post("/register", async (req, res) => {
 });
 
 // ROUTEs
-app.get("/", (req, res) => {
-  res.render(`index.ejs`);
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render(`index.ejs`, { name: req.user.name });
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", checkUnAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.get("/register", (req, res) => {
+app.get("/register", checkUnAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
 console.log(users);
 
+app.delete("/logout", (req, res) => {
+  req.logout(req.user, (err) => {
+    if (err) return next(err);
+    res.redirect("/login");
+  });
+});
+
 // END Routes
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkUnAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
 app.listen(3030);
